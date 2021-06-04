9ca16819bdd61de390c32babd1fbb7ddd0c1973b
Manuel <69605277+Perlera89@users.noreply.github.com>
2021-06-04 13:55:33 -0600

    Comenzando implementacion sucesos

---
+++ Hotel_Vanilla/Hotel_Vanilla/ENTIDADES/ClsESucesos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Vanilla.ENTIDADES
+{
+    class ClsESucesos
+    {
+        public int idSuceso { get; set; }
+        public DateTime? fechaSuceso { get; set; }
+        public string tipoSuceso { get; set; }
+        public string descripcion { get; set; }
+    }
+}
